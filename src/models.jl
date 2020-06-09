39dacbb98d81ac18b008609c94f2a070c5006b63
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2020-06-09 10:46:06 +0200

    Code cleanup and add model equations

--- src/models.jl
+++ src/models.jl
@@ -77,16 +77,15 @@ invalid parameters.
 isvalid(::AbstractPSF) = true
 
 """
-    P = AiryPSF(lim [, eps=0])
+    P = AiryPSF(lim [, ρ=0])
 
 defines the *Point Spread Function* (PSF) `P` of a circular pupil.
 Argument `lim` is the distance in the focal plane corresponding to the
 diffraction limit.  Assuming the distance is given in angular units
 (radians), `lim = λ/D` with `λ` the wavelength and `D` the pupil diameter
-(both expressed in the same units).  Optional argument `eps` is the ratio
-of the size of the central obscuration to that of the pupil (necessarily `0
-≤ eps < 1`).  If not specified, `eps = 0` is assumed (unobstructed pupil).
-Then:
+(both expressed in the same units).  Optional argument `ρ` is the ratio of
+the size of the central obscuration to that of the pupil (necessarily `0 ≤
+ρ < 1`).  If not specified, `ρ = 0` is assumed (unobstructed pupil).  Then:
 
     P([T,] r)
     P([T,] x, y)
@@ -96,74 +95,81 @@ the source and normalized so that the peak value of the PSF is one.  Optional
 argument `T` is the floating-point type of the result.  Arguments `lim`, `r`,
 `x` and `y` are in the same units.
 
+The peak-normalized Airy PSF at distance `r` is given by:
+
+    P(r) = ((a/(c*r))*(J1(c*r) - ρ*J1(ρ*c*r)))^2
+
+with `a = 2/(1 - ρ^2)`, `c = π/lim` and `J1` the Bessel function of the
+first kind.
+
 See also [`CauchyPSF`](@ref), [`GaussianPSF`](@ref), [`MoffatPSF`](@ref).
 
 """ AiryPSF
 
 struct AiryPSF <: AbstractPSF{2}
-    _prm::NTuple{2,Float64}
-    _a::Float64
-    _c::Float64
-    AiryPSF(lim::Real, eps::Real = 0.0) =
-        new((lim, eps),
-            (eps == 0 ? 2.0 : 2.0/((1.0 - eps)*(1.0 + eps))),
-            pi/lim)
+    prm::NTuple{2,Float64}
+    a::Float64
+    c::Float64
+    AiryPSF(lim::Real, rho::Real = 0.0) =
+        new((lim, rho),
+            (rho == 0 ? 2.0 : 2.0/((1.0 - rho)*(1.0 + rho))),
+            π/lim)
 end
 
-@inline isvalid(P::AiryPSF) = _check_airy_lim(P) && _check_airy_eps(P)
+@inline isvalid(P::AiryPSF) = _check_airy_lim(P) && _check_airy_rho(P)
 
 function check_structure(P::AiryPSF)
     _check_airy_lim(P) ||
         throw_bad_argument("bad diffraction limit value")
-    _check_airy_eps(P) ||
+    _check_airy_rho(P) ||
         throw_bad_argument("out of range central obscuration ratio")
 end
 
 @inline _check_airy_lim(P::AiryPSF) = finite_and_positive(_get_lim(P))
-@inline _check_airy_eps(P::AiryPSF) = (e = _get_eps(P);
-                                       isfinite(e) && 0.0 ≤ e < 1.0)
+@inline _check_airy_rho(P::AiryPSF) = (ρ = _get_rho(P);
+                                       isfinite(ρ) && 0.0 ≤ ρ < 1.0)
 
 Base.show(io::IO, P::AiryPSF) =
     print(io, "AiryPSF(", P[1], ",", P[2], ")")
 
-@inline parameters(P::AiryPSF) = getfield(P, :_prm)
-@inline obstrucated(P::AiryPSF) =  _get_eps(P) == 0
+@inline parameters(P::AiryPSF) = getfield(P, :prm)
+@inline obstrucated(P::AiryPSF) =  _get_rho(P) == 0
 
 @inline _get_lim(P::AiryPSF) = @inbounds P[1]
-@inline _get_eps(P::AiryPSF) = @inbounds P[2]
-@inline _get_a(P::AiryPSF) = getfield(P, :_a)
-@inline _get_c(P::AiryPSF) = getfield(P, :_c)
+@inline _get_rho(P::AiryPSF) = @inbounds P[2]
+@inline _get_a(P::AiryPSF) = getfield(P, :a)
+@inline _get_c(P::AiryPSF) = getfield(P, :c)
 
 function getfwhm(P::AiryPSF; kwds...)
-    l, e, a, c =  _get_lim(P), _get_eps(P), _get_a(P), _get_c(P)
+    l, ρ, a, c =  _get_lim(P), _get_rho(P), _get_a(P), _get_c(P)
     v = 1/sqrt(2)
-    f(r) = v - (e == 0 ? _circular_amplitude(a, c, r) :
-                _annular_amplitude(a, c, e, r))
+    f(r) = v - (ρ == 0 ? _circular_amplitude(a, c, r) :
+                _annular_amplitude(a, c, ρ, r))
     return fzero(f, 0.52*l, 0.35*l; kwds...)[1]*2
 end
 
 @inline _circular_amplitude(a::T, c::T, r::T) where {T<:AbstractFloat} =
     r == zero(T) ? one(T) : (u = c*r; (a/u)*J1(u))
 
-@inline _annular_amplitude(a::T, c::T, e::T, r::T) where {T<:AbstractFloat} =
-    r == zero(T) ? one(T) : (u = c*r; (e == zero(T) ?
+@inline _annular_amplitude(a::T, c::T, ρ::T, r::T) where {T<:AbstractFloat} =
+    r == zero(T) ? one(T) : (u = c*r; (ρ == zero(T) ?
                                        (a/u)*J1(u) :
-                                       (a/u)*(J1(u) - e*J1(e*u))))
+                                       (a/u)*(J1(u) - ρ*J1(ρ*u))))
 
 # No central obscuration.
 @inline _airy(a::T, c::T, r::T) where {T<:AbstractFloat} =
     r == zero(T) ? one(T) : (u = c*r; ((a/u)*J1(u))^2)
 
 # With central obscuration.
-@inline _airy(a::T, c::T, e::T, r::T) where {T<:AbstractFloat} =
-    r == zero(T) ? one(T) : (u = c*r; (e == zero(T) ?
+@inline _airy(a::T, c::T, ρ::T, r::T) where {T<:AbstractFloat} =
+    r == zero(T) ? one(T) : (u = c*r; (ρ == zero(T) ?
                                        (a/u)*J1(u) :
-                                       (a/u)*(J1(u) - e*J1(e*u)))^2)
+                                       (a/u)*(J1(u) - ρ*J1(ρ*u)))^2)
 
 (P::AiryPSF)(::Type{T}, r::Real) where {T<:AbstractFloat} =
     _airy(to_float(T, _get_a(P)),
           to_float(T, _get_c(P)),
-          to_float(T, _get_eps(P)),
+          to_float(T, _get_rho(P)),
           to_float(T, r))
 
 (P::AiryPSF)(::Type{T}, x::Real, y::Real) where {T<:AbstractFloat} =
@@ -173,10 +179,10 @@ function (P::AiryPSF)(::Type{T},
                       r::AbstractArray{<:Real}) where {T<:AbstractFloat}
     a = to_float(T, _get_a(P))
     c = to_float(T, _get_c(P))
-    e = to_float(T, _get_eps(P))
-    e == zero(e) ?
+    ρ = to_float(T, _get_rho(P))
+    ρ == zero(ρ) ?
         map(r -> _airy(a, c,    to_float(T, r)), r) :
-        map(r -> _airy(a, c, e, to_float(T, r)), r)
+        map(r -> _airy(a, c, ρ, to_float(T, r)), r)
 end
 
 function (P::AiryPSF)(::Type{T},
@@ -184,11 +190,11 @@ function (P::AiryPSF)(::Type{T},
                       y::AbstractArray{<:Real}) where {T<:AbstractFloat}
     a = to_float(T, _get_a(P))
     c = to_float(T, _get_c(P))
-    e = to_float(T, _get_eps(P))
-    e == zero(e) ?
+    ρ = to_float(T, _get_rho(P))
+    ρ == zero(ρ) ?
         broadcast((x, y) -> _airy(a, c,    hypot(to_float(T, x), to_float(T, y))),
                   x, y) :
-        broadcast((x, y) -> _airy(a, c, e, hypot(to_float(T, x), to_float(T, y))),
+        broadcast((x, y) -> _airy(a, c, ρ, hypot(to_float(T, x), to_float(T, y))),
                   x, y)
 end
 
@@ -226,11 +232,11 @@ end
 function findzeros(::Type{T}, P::AiryPSF, n::Integer;
                    kwds...) where {T<:AbstractFloat}
     @assert n ≥ 1
-    l, e, a, c = T(_get_lim(P)), T(_get_eps(P)), T(_get_a(P)), T(_get_c(P))
-    g = (n + T(0.25) - T(0.49)*e)*l  # guess for the n-th zero
+    l, ρ, a, c = T(_get_lim(P)), T(_get_rho(P)), T(_get_a(P)), T(_get_c(P))
+    g = (n + T(0.25) - T(0.49)*ρ)*l  # guess for the n-th zero
     h = T(0.7)*l                     # half-width of search interval
     obstrucated(P) ?
-        fzero(r -> _annular_amplitude(a, c, e, r), g - h, g + h; kwds...)[1] :
+        fzero(r -> _annular_amplitude(a, c, ρ, r), g - h, g + h; kwds...)[1] :
         fzero(r -> _circular_amplitude(a, c, r),   g - h, g + h; kwds...)[1]
 end
 
@@ -248,13 +254,19 @@ position of the source and normalized so that the peak value of the PSF is
 one.  Optional argument `T` is the floating-point type of the result.
 Arguments `fwhm`, `r`, `x` and `y` are in the same units.
 
+The peak-normalized Cauchy PSF at distance `r` is given by:
+
+    P(r) = q/(q + r^2)
+
+with `q = (fwhm/2)^2`.
+
 See also [`AiryPSF`](@ref), [`GaussianPSF`](@ref), [`MoffatPSF`](@ref).
 
 """ CauchyPSF
 
 struct CauchyPSF <: AbstractPSF{1}
     fwhm::Float64
-    _q::Float64
+    q::Float64
     CauchyPSF(fwhm::Real) = new(fwhm, (fwhm/2)^2)
 end
 
@@ -269,7 +281,7 @@ Base.show(io::IO, P::CauchyPSF) =
 @inline getfwhm(P::CauchyPSF) = getfield(P, :fwhm)
 @inline parameters(P::CauchyPSF) = (getfwhm(P),)
 
-@inline _get_q(P::CauchyPSF) = getfield(P, :_q)
+@inline _get_q(P::CauchyPSF) = getfield(P, :q)
 
 @inline _cauchy(q::T, r::T) where {T<:AbstractFloat} = q/(r*r + q)
 
@@ -313,13 +325,19 @@ position of the source and normalized so that the peak value of the PSF is
 one.  Optional argument `T` is the floating-point type of the result.
 Arguments `fwhm`, `r`, `x` and `y` are in the same units.
 
+The peak-normalized Gaussian PSF at distance `r` is given by:
+
+    P(r) = exp(q*r^2)
+
+with `q = -4*log(2)/fwhm^2`.
+
 See also [`AiryPSF`](@ref), [`CauchyPSF`](@ref), [`MoffatPSF`](@ref).
 
 """ GaussianPSF
 
 struct GaussianPSF <: AbstractPSF{1}
     fwhm::Float64
-    _q::Float64
+    q::Float64
     GaussianPSF(fwhm::Real) = new(fwhm, -log(2.0)*(2.0/fwhm)^2)
 end
 
@@ -334,7 +352,7 @@ Base.show(io::IO, P::GaussianPSF) =
 @inline getfwhm(P::GaussianPSF) = getfield(P, :fwhm)
 @inline parameters(P::GaussianPSF) = (getfwhm(P),)
 
-@inline _get_q(P::GaussianPSF) = getfield(P, :_q)
+@inline _get_q(P::GaussianPSF) = getfield(P, :q)
 
 @inline _gauss(q::T, r::T) where {T<:AbstractFloat} = exp(r*r*q)
 
@@ -382,16 +400,22 @@ position of the source and normalized so that the peak value of the PSF is
 one.  Optional argument `T` is the floating-point type of the result.
 Arguments `fwhm`, `r`, `x` and `y` are in the same units.
 
+The peak-normalized Moffat PSF at distance `r` is given by:
+
+    P(r) = (1 + q*r^2)^(-beta)
+
+with `q = (2^(1/beta) - 1)*(2/fwhm)^2`.
+
 See also [`AiryPSF`](@ref), [`CauchyPSF`](@ref), [`GaussianPSF`](@ref).
 
 """ MoffatPSF
 
 struct MoffatPSF <: AbstractPSF{2}
-    _prm::NTuple{2,Float64}
-    _p::Float64
-    _q::Float64
+    prm::NTuple{2,Float64} # (fwhm, beta)
+    p::Float64 # (2^(1/beta) - 1)*(2/fwhm)^2
+    q::Float64 # -beta
     MoffatPSF(fwhm::Real, beta::Real) =
-        new((fwhm, beta), -beta, (2^(1/beta) - 1)*(2/fwhm)^2)
+        new((fwhm, beta), -beta, (2.0^(1.0/beta) - 1.0)*(2.0/fwhm)^2)
 end
 
 @inline isvalid(P::MoffatPSF) =
@@ -408,12 +432,12 @@ Base.show(io::IO, P::MoffatPSF) =
     print(io, "MoffatPSF(", P[1], ",", P[2], ")")
 
 @inline getfwhm(P::MoffatPSF) = _get_fwhm(P)
-@inline parameters(P::MoffatPSF) =  getfield(P, :_prm)
+@inline parameters(P::MoffatPSF) =  getfield(P, :prm)
 
 @inline _get_fwhm(P::MoffatPSF) = @inbounds P[1]
 @inline _get_beta(P::MoffatPSF) = @inbounds P[2]
-@inline _get_p(P::MoffatPSF) = getfield(P, :_p)
-@inline _get_q(P::MoffatPSF) = getfield(P, :_q)
+@inline _get_p(P::MoffatPSF) = getfield(P, :p)
+@inline _get_q(P::MoffatPSF) = getfield(P, :q)
 
 @inline _moffat(p::T, q::T, r::T) where {T<:AbstractFloat} =
     (r*r*q + one(T))^p
